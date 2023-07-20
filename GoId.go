bcc2c7ecd3bacefea26014bcbd81f9de94e33275
AnimusPEXUS <animuspexusagutilities@gmail.com>
2023-07-20 16:31:05 +0300

    GetCurrentGoId -> GetCurrentGoId_byRuntimeStack

--- GoId.go
+++ GoId.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func GetCurrentGoId() (uint64, error) {
+func GetCurrentGoId_byRuntimeStack() (uint64, error) {
 	b := make([]byte, 1024)
 	runtime.Stack(b, false)
 	return strconv.ParseUint(string(bytes.Fields(b)[1]), 10, 64)
